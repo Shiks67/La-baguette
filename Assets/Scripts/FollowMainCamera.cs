7d27e6a2b1b778fd1e53408c0802c9df4ff0876c
Arthur <ahmed.sdlv@gmail.com>
2018-12-03 10:56:46 +0100

    +RayCaster / desktop camera following main

---
+++ Assets/Scripts/FollowMainCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMainCamera : MonoBehaviour {
+
+	private Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+		mainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.rotation = mainCamera.transform.rotation;
+		gameObject.transform.position = mainCamera.transform.position;
+	}
+}
---
+++ Assets/Scripts/RayCaster.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCaster : MonoBehaviour
+{
+
+    private Camera mainCamera;
+    private Vector3 viewportPoint;
+    private Vector2 gazePointCenter;
+    private RaycastHit[] headHits;
+
+    private RaycastHit[] gazeHits;
+    private Ray gazeRay;
+    private Ray headRay;
+
+    // Use this for initialization
+    void Start()
+    {
+        PupilData.calculateMovingAverage = true;
+        mainCamera = Camera.main;
+
+        if (PupilTools.IsConnected)
+            PupilGazeTracker.Instance.StartVisualizingGaze();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        viewportPoint = new Vector3(0.5f, 0.5f, 10);
+        if (PupilTools.IsConnected && PupilTools.IsGazing)
+        {
+
+            gazePointCenter = PupilData._2D.GazePosition;
+            viewportPoint = new Vector3(gazePointCenter.x, gazePointCenter.y, 1f);
+        }
+        gazeRay = mainCamera.ViewportPointToRay(viewportPoint);
+
+        headRay = new Ray(mainCamera.transform.position, mainCamera.transform.rotation * Vector3.forward);
+        headHits = Physics.RaycastAll(headRay);
+        for (int i = 0; i < headHits.Length; i++)
+        {
+            RaycastHit hit = headHits[i];
+            if (hit.transform.name == "Table")
+            {
+                var headPosLocal = hit.transform.InverseTransformPoint(hit.point);
+                // print(headPosLocal);
+            }
+        }
+
+        gazeHits = Physics.RaycastAll(gazeRay);
+        for (int i = 0; i < gazeHits.Length; i++)
+        {
+            RaycastHit hit = gazeHits[i];
+            if (hit.transform.name == "Baguette")
+            {
+                var gazePosLocal = hit.transform.InverseTransformPoint(hit.point);
+                // print(gazePosLocal);
+            }
+        }
+    }
+}
