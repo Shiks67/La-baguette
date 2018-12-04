c319d31478419f678daf6506839bd82206b0728c
Arthur <ahmed.sdlv@gmail.com>
2018-12-04 12:33:32 +0100

    + different modes

---
+++ Assets/Scripts/EyePatch/EyePatchManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EyePatchManager : MonoBehaviour
+{
+    public void EyePatchRight()
+    {
+        gameObject.transform.localPosition = new Vector3(0.1f, 0, 0);
+    }
+
+    public void EyePatchLeft()
+    {
+        gameObject.transform.localPosition = new Vector3(-0.1f, 0, 0);
+    }
+}
