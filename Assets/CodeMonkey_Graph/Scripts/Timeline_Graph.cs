bd72e874f50084db1ab430d04a7e9e7daae22912
letalskyppy <alexandre.monnier@viacesi.fr>
2019-02-04 12:01:45 +0100

    Update Timeline graph

--- Assets/CodeMonkey_Graph/Scripts/Timeline_Graph.cs
+++ Assets/CodeMonkey_Graph/Scripts/Timeline_Graph.cs
@@ -9,17 +9,62 @@ public class Timeline_Graph : MonoBehaviour {
 
 	[SerializeField] private Sprite circleSprite;
 	private RectTransform graphContainer;
+	private RectTransform labelTemplateY;
 
+	private RectTransform timeAxisTemplate;
+	private RectTransform timeAxis;
+	private RectTransform dashContainer;
 	private RectTransform graph;
 	private List<GameObject> gameObjectList;
+
+	private Scrollbar scrollbar;
+	public int offset;
+	private int timer;
 	private void Awake(){
+		scrollbar = transform.Find("scrollbar").GetComponent<Scrollbar>();
+		scrollbar.gameObject.SetActive(false);
 		gameObjectList = new List<GameObject>();
+		timer = 0;
 		graphContainer = transform.Find("graphContainer").GetComponent<RectTransform>();
 		graph = transform.GetComponent<RectTransform>();
+		dashContainer = graphContainer.Find("dashContainer").GetComponent<RectTransform>();
+		labelTemplateY = graphContainer.Find("labelTemplateY").GetComponent<RectTransform>();
+		timeAxisTemplate = dashContainer.Find("dashTemplateY").GetComponent<RectTransform>();
 
-		List<double> headList = new List<double>(){-5,-4,-2,2,3,1,5,0,0,0,0,2,4,1,-4,-5,-2,-1,-1,-1,-1};
-		List<double> eyeList = new List<double>(){-2,-2,-3,0,2,-1,5,1,0,0,2,4,5,0,-2,-3,-1,0,0,0,0};
+		timeAxis = Instantiate(timeAxisTemplate);
+		timeAxis.SetParent(graphContainer,false);
+		timeAxis.gameObject.SetActive(true);
+		timeAxis.anchoredPosition = new Vector2(-1.96f,graphContainer.sizeDelta.y/2);
+
+		List<double> headList = new List<double>(){};
+		List<double> eyeList = new List<double>(){};
 		ShowGraph(headList, eyeList);
+
+		for(int i = -6; i <= 6; i++){
+			RectTransform labelY = Instantiate(labelTemplateY);
+			labelY.SetParent(graphContainer,false);
+			labelY.gameObject.SetActive(true);
+			labelY.anchoredPosition = new Vector2(-5f, (i/12f) * graphContainer.sizeDelta.y + graphContainer.sizeDelta.y/2 + 4f);
+			labelY.GetComponent<Text>().text = i.ToString();
+
+			RectTransform labelDown = Instantiate(labelTemplateY);
+			labelDown.SetParent(graphContainer,false);
+			labelDown.gameObject.SetActive(true);
+			labelDown.anchoredPosition = new Vector2(graphContainer.sizeDelta.x+10f, (i/12f) * graphContainer.sizeDelta.y + graphContainer.sizeDelta.y/2 + 4f);
+			labelDown.GetComponent<Text>().text = i.ToString();
+			if(i == 6){
+				labelY.GetComponent<Text>().text = "5+";
+				labelY.GetComponent<Text>().color = Color.red;
+				labelDown.GetComponent<Text>().text = "5+";
+				labelDown.GetComponent<Text>().color = Color.red;
+			}
+			if(i == -6){
+				labelY.GetComponent<Text>().text = "-5+";
+				labelY.GetComponent<Text>().color = Color.red;
+				labelDown.GetComponent<Text>().text = "-5+";
+				labelDown.GetComponent<Text>().color = Color.red;
+			}	
+		}
 
 		FunctionPeriodic.Create(() => {
 			double x = 0;
@@ -68,7 +113,39 @@ public class Timeline_Graph : MonoBehaviour {
             }
 			ShowGraph(headList, eyeList);
         }, .2f);
+		
+		/* FunctionPeriodic.Create(() => { 
+			float xSize = 10.7f;
+			timer++;
+			GameObject labelTimeline = new GameObject("labelTimeline");
+			labelTimeline.transform.SetParent(graphContainer,false);
+			RectTransform timeline = Instantiate(labelTemplateY);
+			timeline.SetParent(labelTimeline.transform,false);
+			timeline.gameObject.SetActive(true);
+			timeline.GetComponent<Text>().text = DisplayTime(timer);
+			timeline.anchoredPosition = new Vector2(xSize + (timer*5) * xSize,graphContainer.sizeDelta.y/2);
+		}, 1f);*/
+	}
 
+	private String DisplayTime(int timer){
+		int secondes = timer % 60;
+		double tg = timer / 60;
+		int minutes = (int)Math.Floor(tg);
+		String minutesString = "";
+		String secondesString ="";
+		if(minutes < 10){
+			minutesString = "0"+minutes.ToString()+":";
+		}	
+		else{
+			minutesString = minutes.ToString();
+		}
+		if(secondes < 10){
+			secondesString = "0"+secondes.ToString();
+		}else{
+			secondesString = secondes.ToString();
+		}
+		 
+		return minutesString + secondesString;
 	}
 
 	private GameObject CreateCircle(Vector2 anchoredPosition, Color dotColor){
@@ -96,33 +173,106 @@ public class Timeline_Graph : MonoBehaviour {
             Destroy(gameObject);
          }
 		 gameObjectList.Clear();
+		
+		int NbMaxDisplayed = 46;
+		int NbDataForOneSecond = 5;
 
-		for(int i = 0; i < eyeList.Count && i < headList.Count; i++){
-			float xPosition = xSize + i * xSize;
-			float yPosition = ((float)eyeList[i]/ yMaximum) * graphHeight + graphHeight/2;
-			Vector2 eyePos = new Vector2(xPosition, yPosition);
-			float xPositionH = xSize + i * xSize;
-			float yPositionH = ((float)headList[i]/ yMaximum) * graphHeight + graphHeight/2;
-			Vector2 headPos = new Vector2(xPositionH, yPositionH);
-			if(xPosition > graphWidth-10){
-				graphContainer.sizeDelta = new Vector2(graphWidth + 20, graphHeight);
-				graph.sizeDelta = new Vector2(graphMainWidth + 20, graphMainHeight);
-			}
+		if(eyeList.Count < NbMaxDisplayed || headList.Count < NbMaxDisplayed){
+			for(int i = 0; i < eyeList.Count || i < headList.Count; i++){
+				float xPosition = xSize + i * xSize;
+				float yPosition = ((float)eyeList[i]/ yMaximum) * graphHeight + graphHeight/2;
+				Vector2 eyePos = new Vector2(xPosition, yPosition);
+				float xPositionH = xSize + i * xSize;
+				float yPositionH = ((float)headList[i]/ yMaximum) * graphHeight + graphHeight/2;
+				Vector2 headPos = new Vector2(xPositionH, yPositionH);
+				/* if(xPosition > graphWidth-10){
+					graphContainer.sizeDelta = new Vector2(graphWidth + 20, graphHeight);
+					graph.sizeDelta = new Vector2(graphMainWidth + 20, graphMainHeight);
+					timeAxis.sizeDelta = new Vector2(timeAxis.sizeDelta.x + 20, timeAxis.sizeDelta.y);
+				}*/
 				
-			if(eyeList[i] > headList[i])
-				CreateDotConnection(headPos, eyePos, Color.blue);
-			if(eyeList[i] < headList[i])
-				CreateDotConnection(headPos, eyePos, Color.magenta);
-			if(eyeList[i] == headList[i])
-				CreateDotConnection(headPos, eyePos, Color.white);
+				if(i % 5 == 0){
+					int timer = i/NbDataForOneSecond;
 
-			GameObject eyeGameObject = CreateCircle(eyePos, Color.red);
-			GameObject headGameObject = CreateCircle(headPos, Color.grey);
-			
-			gameObjectList.Add(eyeGameObject);
-			gameObjectList.Add(headGameObject);
+					GameObject labelTimeline = new GameObject("labelTimeline");
+					labelTimeline.transform.SetParent(graphContainer,false);
+					RectTransform timeline = Instantiate(labelTemplateY);
+					timeline.SetParent(labelTimeline.transform,false);
+					timeline.gameObject.SetActive(true);
+					timeline.GetComponent<Text>().text = DisplayTime(timer);
+					timeline.anchoredPosition = new Vector2(xSize + i * xSize,graphContainer.sizeDelta.y + 45f);
+					gameObjectList.Add(labelTimeline);
+				}
 
+				if(eyeList[i] > headList[i])
+					CreateDotConnection(headPos, eyePos, Color.blue);
+				if(eyeList[i] < headList[i])
+					CreateDotConnection(headPos, eyePos, Color.magenta);
+				if(eyeList[i] == headList[i])
+					CreateDotConnection(headPos, eyePos, Color.white);
+
+				GameObject eyeGameObject = CreateCircle(eyePos, Color.red);
+				GameObject headGameObject = CreateCircle(headPos, Color.grey);
+				
+				gameObjectList.Add(eyeGameObject);
+				gameObjectList.Add(headGameObject);
+			}
+		}else{			
+			scrollbar.gameObject.SetActive(true);
+
+			int iStartValue = eyeList.Count < headList.Count ? eyeList.Count - NbMaxDisplayed : headList.Count - NbMaxDisplayed;
+			int iMaxValue = eyeList.Count < headList.Count ? eyeList.Count : headList.Count;
+			double offsetValueDouble = Math.Floor(scrollbar.value*100) / 100 * iMaxValue;
+			offset = (int)Math.Floor(offsetValueDouble);
+			iStartValue = iStartValue - offset >= 0 ? iStartValue - offset : 0;
+			iMaxValue = iMaxValue - offset >= iStartValue + NbMaxDisplayed ? iMaxValue - offset : iStartValue + NbMaxDisplayed;
+
+			int cpt = 0;
+
+			for(int i = iStartValue; i < iMaxValue; i++){
+				float xPosition = xSize + cpt * xSize;
+				float yPosition = ((float)eyeList[i]/ yMaximum) * graphHeight + graphHeight/2;
+				Vector2 eyePos = new Vector2(xPosition, yPosition);
+				float xPositionH = xSize + cpt * xSize;
+				float yPositionH = ((float)headList[i]/ yMaximum) * graphHeight + graphHeight/2;
+				Vector2 headPos = new Vector2(xPositionH, yPositionH);
+				/* if(xPosition > graphWidth-10){
+					graphContainer.sizeDelta = new Vector2(graphWidth + 20, graphHeight);
+					graph.sizeDelta = new Vector2(graphMainWidth + 20, graphMainHeight);
+					timeAxis.sizeDelta = new Vector2(timeAxis.sizeDelta.x + 20, timeAxis.sizeDelta.y);
+				}*/
+				
+				if(cpt % 5 == 0){
+					int timer = (cpt + iStartValue)/NbDataForOneSecond;
+
+					GameObject labelTimeline = new GameObject("labelTimeline");
+					labelTimeline.transform.SetParent(graphContainer,false);
+					RectTransform timeline = Instantiate(labelTemplateY);
+					timeline.SetParent(labelTimeline.transform,false);
+					timeline.gameObject.SetActive(true);
+					timeline.GetComponent<Text>().text = DisplayTime(timer);
+					timeline.anchoredPosition = new Vector2(xSize + cpt * xSize,graphContainer.sizeDelta.y + 40f);
+					gameObjectList.Add(labelTimeline);
+				}
+
+
+				if(eyeList[i] > headList[i])
+					CreateDotConnection(headPos, eyePos, Color.blue);
+				if(eyeList[i] < headList[i])
+					CreateDotConnection(headPos, eyePos, Color.magenta);
+				if(eyeList[i] == headList[i])
+					CreateDotConnection(headPos, eyePos, Color.white);
+
+				GameObject eyeGameObject = CreateCircle(eyePos, Color.red);
+				GameObject headGameObject = CreateCircle(headPos, Color.grey);
+				
+				gameObjectList.Add(eyeGameObject);
+				gameObjectList.Add(headGameObject);
+				cpt++;
+			}
 		}
+		
+		//CreateLabelYDown();
 	}
 
 	private void CreateDotConnection(Vector2 dotPositionA, Vector2 dotPositionB, Color dotConnectionColor){
@@ -138,5 +288,14 @@ public class Timeline_Graph : MonoBehaviour {
 		rectTransform.anchoredPosition = dotPositionA + dir * distance * .5f;
 		rectTransform.localEulerAngles = new Vector3(0,0,UtilsClass.GetAngleFromVectorFloat(dir));
 		gameObjectList.Add(gameObject);
+	}
+
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.UpArrow)){
+			graph.position = new Vector3(graph.position.x,graph.position.y-40,graph.position.z);
+		}
+		if(Input.GetKeyDown(KeyCode.DownArrow)){
+			graph.position = new Vector3(graph.position.x,graph.position.y+40,graph.position.z);
+		}
 	}
 }
